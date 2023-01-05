6274bbb69510fdd10adeee4a19cc9bbb8eb3ed73
somya143 <maaheersj@gmail.com>
2023-01-05 10:31:23 +0530

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
+
 server.use(router)
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
